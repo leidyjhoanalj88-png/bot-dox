18aadddc30a0e08d9c8e2749ca9733e49a4eb0b5
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-03-06 08:42:41 -0500

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,4 @@
 
-# Detener el bot
-Ctrl + C
-
-# Volver a iniciar
-
-
 # ══════════════════════════════════════════════════════════════════
 # INSTRUCCIONES:
 # Abre tu bot.py y haz estos 3 cambios:
